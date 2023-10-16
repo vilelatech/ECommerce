95802dda39bf78211560270c72fb5119f32527eb
Bruno Vilela <bruno@vilelatech.com>
2023-10-16 11:24:49 -0300

    Projeto inicial

---
+++ ECommerce/ECommerce.Infra/Data/StoreContext .cs
@@ -0,0 +1,13 @@
+﻿using ECommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Infra.Data;
+
+public class StoreContext : DbContext
+{
+    public StoreContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+}
